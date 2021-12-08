70cb243710c2fe4655e5acac4007df392028fa53
PerseusEons <percyeons@gmail>
2021-12-08 06:20:57 -0800

    first

---
+++ build/CMakeFiles/RedNoise.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/asa/Desktop/RedNoise/libs/sdw/CanvasPoint.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/CanvasPoint.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/CanvasTriangle.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/CanvasTriangle.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/Colour.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/Colour.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/DrawingWindow.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/DrawingWindow.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/ModelTriangle.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/ModelTriangle.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/RayTriangleIntersection.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/RayTriangleIntersection.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/TextureMap.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/TextureMap.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/TexturePoint.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/TexturePoint.cpp.o"
+  "/home/asa/Desktop/RedNoise/libs/sdw/Utils.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/libs/sdw/Utils.cpp.o"
+  "/home/asa/Desktop/RedNoise/src/RedNoise.cpp" "/home/asa/Desktop/RedNoise/build/CMakeFiles/RedNoise.dir/src/RedNoise.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  "../libs/glm-0.9.7.2"
+  "../libs/sdw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
