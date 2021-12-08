7c00dd40f0bfe38aca69b2b43ebd76e87f5eaf0e
PerseusEons <percyeons@gmail>
2021-12-08 06:08:16 -0800

    first

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -1,13 +1,23 @@
 #include <CanvasTriangle.h>
+#include <ModelTriangle.h>
+#include <CanvasPoint.h>
+#include <TextureMap.h>
+#include <Colour.h>
 #include <DrawingWindow.h>
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <unordered_map>
 
 #define WIDTH 320
 #define HEIGHT 240
 
-std::vector<float>
+using namespace std;
+using namespace glm;
+
+vec3 camera(0.0,0.0,4.0)
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
@@ -21,6 +31,41 @@ void draw(DrawingWindow &window) {
 		}
 	}
 }
+//nov numberofvalues
+std::vector<CanvasPoint> interpolate(CanvasPoint from, CanvasPoint to, int nov){
+	std::vector<CanvasPoint> cp;
+	float xNov = (to.x - from.x)/(nov - 1);
+	float yNov = (to.y - from.y)/(nov - 1);
+	CanvasPoint v;
+
+	for(float i = 0; i < nov -1; i++){
+		v.x = v.x + x_nov;
+		v.y = v.y + y_nov;
+		cp.push_back(v);
+	}
+	return cp;
+
+
+}
+
+void drawLine(CanvasPoint from, CanvasPoint to, glm::vec3 colour, DrawingWindow& window ) {
+	float xDiff = to.x - from.x;
+	float yDiff = to.y - from.y;
+	float nos =  std::max(abs(xDiff), abs(yDiff));
+	float xStepSize = xDiff/nos;
+	float yStepSize = yDiff/nos;
+
+	uint32_t col = (255 << 24) + ((int)colour[0] << 16) + ((int)colour[1] << 8) + (int)colour[2];
+	for (float i=0.0; i<nos; i++){
+		float x = from.x + (xStepSize * i);
+		float y = from.y + (yStepSize * i);
+		window.setPixelColour(round(x), round(y), col)
+
+	}
+
+}
+
+
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
 	if (event.type == SDL_KEYDOWN) {
